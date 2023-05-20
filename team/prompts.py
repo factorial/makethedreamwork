839f2e98cf45da9a97f4e2214868f0f92593a1f1
factorial <johnphayes@gmail.com>
2023-05-20 22:27:54 +0000

    decent moderator prompt

--- team/prompts.py
+++ team/prompts.py
@@ -26,15 +26,16 @@ AVATAR_FROM_CITY = "3D rendered cartoon avatar of {mascfem} {add} {role} from {c
 
 AVATAR = "3D rendered cartoon avatar of {mascfem} person, highlight hair, centered, studio lighting, looking at the camera, dslr, ultra quality, sharp focus, tack sharp, dof, Fujifilm XT3, crystal clear, 8K UHD, highly detailed glossy eyes, high detailed skin, skin pores, international, NOT ugly, NOT disfigured, NOT bad"
 
-AI_ROLE_PROMPT = """You are the expert {role} on this team. Team objective: '{objective}'.
+AI_ROLE_PROMPT = """You are the helpful expert {role} on this team Team objective: '{objective}'.
 You follow the Moderator's instructions.
 You respond with highly detailed information lists, examples, and step-by-step task lists.
+Occasionally you ask for information from another team member.
 """
 #Your responsibilities are:{responsibilities}"""
 
 MODERATOR_AVATAR = "3D rendered {mascfem} silhouette, centered, studio lighting, looking directly at the camera, dslr, ultra quality, sharp focus, tack sharp, dof, Fujifilm XT3, crystal clear, 8K UHD, high detail, NOT ugly, NOT disfigured, NOT bad"
 
 
-SUMMARIZER = """Generate a highly detailed report of this team's findings and current tasks so that a new team can take over and pick up where this team left off."""
+SUMMARIZER = """Generate a highly detailed report of findings and current tasks found within a group discussion so that a new team can take over and pick up where this team left off."""
 
-TASK_FINDER = """You are an information request finder called Moderator. Find one request for information, if any, in this chat log that {role} can answer and direct {role} to give a detailed response right now. If {role} has no tasks, remain silent. Be direct and concise, to the point. You absolutely never say the phrase "AI language model"."""
+TASK_FINDER = """You are an information request finder named Moderator. Find one request for information in this chat log that a person in the role of '{role}' can answer. Ask {role} to give a detailed answer. If the {role} has no tasks, ask {role} to speak. You are very brief and concise."""
